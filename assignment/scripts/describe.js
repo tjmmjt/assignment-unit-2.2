c43d1cd597abc0c7bb84f6953841e6cd5ec420eb
Tucker Melssen <tuckerjmelssen@gmail.com>
2023-11-01 12:39:31 -0500

    assignment complete

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,10 +26,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// The variable 'name' is declared and is set equal to the string 'Dane'
+// Then a conditional statement is written. If the variable name is equal to 'Mary', it will console.log 'Hi, Mary!'
+// If the variable name equals anything other than 'Mary' then it will console.log 'How do you do?'
+// In this scenario, it will console.log 'How do you do?' because the variable is equal to 'Dane'
 
 //CODE
 /*
@@ -45,10 +45,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// The variable 'secret' is declared.
+// The variable 'code' is declared and set equal to '123'
+// A conditional statement is written as such. If the variable 'code' is equal to 123, then the variable 'secret' will be set equal to the string 'super' and the variable 'code' will be reassigned to itself multiplied by 2
+// Another conditional statement is written. If the variable 'code' is greater than the value 250, then the variable 'secret' will be set equal to the string 'duper'
+// console.log(secret) will log 'super' because code === 123 in the first conditional, and = 246 (less than 250) in the second conditional.
 
 //CODE
 /*
@@ -70,10 +71,13 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// The variables 'isStudent', 'age', and 'zip' are declared and set equal to the boolean value 'true', the number value '34', and number value '55407'
+// A compound conditional is written with the following conditions: 
+// If the variable 'isStudent' is true AND 'zip' is greater than 80000, it will log 'You're a student on the West Coast!' to the console.
+// If isStudent is false OR age is less than 30, then it will log 'What are your hobbies?' to the console.
+// If isStudent is true, it will log 'Welcome to Prime!' to the console.
+// For all other conditions, it will log 'How about the weather?' to the console. 
+// In this example, it will log 'Welcome to Prime!' to the console.
 
 //CODE
 /*
@@ -129,11 +133,13 @@ if (number >= 2) {
 // colorTwo is set to 'red', and mix is set to true. We check if mix is true 
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
+// FIX - colorOne should be set to blue and colorTwo should be set to red. 
 /*
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
 
+// FIX - Missing "colorTwo = 'purple'" in conditional statement
 if (mix === true) {
   colorOne = 'purple';
 }
@@ -143,10 +149,12 @@ if (mix === true) {
 // We check if temp is higher than 39 and if time is greater or equal to  4 
 // -- they are so we console.log 'throw away the food!'
 
+// FIX - time should be declared with let instead of const because time is never constant. 
 /*
 let temp = 40;
 const time = 4;
 
+// FIX - || is the 'or' operator and should be replaced with the 'and' operator &&
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
@@ -160,6 +168,7 @@ if (temp > 39 || time >= 4) {
 let age = 21;
 const minAge = 21;
 
+// FIX - if/else statements are switched. The first statement should be console.log('enter'), and the else statement should be console.log('no entry')
 if(minAge <= age) {
   console.log('no entry');
 } else {
